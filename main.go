6a01444e693ca4c21ef18bd9baf7e77a19af77ee
MagomedovMurad20 <magdev09@gmail.com>
2024-07-18 20:02:34 +0300

    вынес подключение к базе и роуты в отдельные файлы

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"api/handlers"
-	"database/sql"
+	"api/database"
+	"api/routes"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -11,18 +11,13 @@ import (
 func main() {
 	router := gin.Default()
 
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/aa")
+	db, err := database.Connect()
 	if err != nil {
 		panic("Failed to connect to database")
 	}
 	defer db.Close()
 
-	UserHandler := handlers.UserHandler{Db: db}
+	routes.SetupRoutes(router, db)
 
-	router.GET("/user", UserHandler.GetAllUsers)
-	router.GET("/user/:id", UserHandler.GetUserById)
-	router.POST("/user", UserHandler.CreateUser)
-	router.PUT("/updateuser/:id", UserHandler.UpdateUser)
-	router.DELETE("/deleteuser/:id", UserHandler.DeleteUserById)
 	router.Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,17 +1,18 @@
 package routes
 
-// import (
-// 	"api/handlers"
+import (
+	"api/handlers"
+	"database/sql"
 
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/gin-gonic/gin"
+)
 
-// func Routes() {
-// 	router := gin.Default()
-// 	router.GET("/bills", handlers.GetBills)
-// 	router.GET("/bills/:id", handlers.GetBillBySurname)
-// 	router.DELETE("/bills/:id", handlers.DeleteBillBySurname)
-// 	router.POST("/updateBills/:id", handlers.UpdateBillBySurname)
-// 	router.POST("/bills", handlers.PostBills)
-// 	router.Run("localhost:8080")
-// }
+func SetupRoutes(router *gin.Engine, db *sql.DB) {
+	userHandler := handlers.UserHandler{Db: db}
+
+	router.GET("/user", userHandler.GetAllUsers)
+	router.GET("/user/:id", userHandler.GetUserById)
+	router.POST("/user", userHandler.CreateUser)
+	router.PUT("/updateuser/:id", userHandler.UpdateUser)
+	router.DELETE("/deleteuser/:id", userHandler.DeleteUserById)
+}
